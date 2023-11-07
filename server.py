dd64dc69a949cb173c62627a134213598fc811cf
Pedro Maia <pedrommaiaa@protonmail.com>
2023-11-07 17:09:54 -0500

    Add support for multiple commands

--- server.py
+++ server.py
@@ -1,5 +1,23 @@
 import socket
 
+def read_command(buffer):
+    if b"\r\n" in buffer:
+        parts = buffer.split(b"\r\n", 1)
+        if parts[0] == b"*1":
+            length_command = parts[1].split(b"\r\n", 1)
+            length = int(length_command[0].lstrip(b"$"))
+            if len(length_command[1]) >= length + 2:
+                command_data = length_command[1][:length].lower()
+                remaining_buffer = length_command[1][length + 2:]
+                return command_data, remaining_buffer
+            else:
+                return None, buffer
+        else:
+            return None, b""
+    else:
+        return None, buffer
+
+
 def main():
     server = socket.create_server(("localhost", 6379), reuse_port=True)
     
@@ -14,15 +32,15 @@ def main():
         
         buffer += data
 
-        if b"\r\n" in buffer:
-            lines = buffer.split(b"\r\n")
+        while b"\r\n" in buffer:
+            command, buffer = read_command(buffer)
+            if command is not None:
+                if command == b"ping":
+                    conn.sendall(b"+PONG\r\n")
+                else:
+                    conn.sendall(b"-ERR unkown command\r\n")
+                    buffer = b""
 
-            if lines[0] == b"*1" and lines[1] == b"$4" and lines[2].lower() == b"ping":
-                conn.sendall(b"+PONG\r\n")
-                break
-            
-            buffer = lines[-1]
-    
     conn.close()
     server.close()
 
