1d4564648307b74eb64b41756f9469922d903c27
Pedro Maia <pedrommaiaa@protonmail.com>
2023-12-05 17:43:24 -0500

    Minimal, only PING

--- jet/jet.py
+++ jet/jet.py
@@ -1,6 +1,64 @@
 import asyncio
-from .commands import CommandFactory
-from .command_parser import CommandParser
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    def __init__(self, args):
+        self.args = args
+
+    @abstractmethod
+    def execute(self, writer):
+        pass
+
+class PingCommand(Command):
+    def execute(self, writer):
+        writer.write(b"+PONG\r\n")
+
+class CommandFactory:
+    @staticmethod
+    def get_command(command_name, args):
+        if command_name.lower() == b"ping":
+            return PingCommand(args)
+        return None
+
+class CommandParser:
+    def __init__(self):
+        self.buffer = b""
+
+    def add_data(self, data):
+        self.buffer += data
+
+    def has_command(self):
+        return b"\r\n" in self.buffer
+
+    def parse_command(self):
+        if not self.buffer:
+            return None
+        
+        lines = self.buffer.split(b"\r\n")
+        if lines[0].startswith(b"*"):
+            num_args = int(lines[0][1:])
+            command_parts = []
+            idx = 1
+
+            while num_args > 0 and idx < len(lines):
+                if lines[idx].startswith(b"$"):
+                    length = int(lines[idx][1:])
+                    idx += 1
+                    if idx < len(lines):
+                        bulk_string = lines[idx]
+                        if len(bulk_string) != length:
+                            return None
+                        command_parts.append(bulk_string)
+                        idx += 1
+                    else:
+                        return None
+                else:
+                    return None
+                num_args -= 1
+            
+            self.buffer = b"\r\n".join(lines[idx:])
+            return command_parts
+        return None
 
 class Jet:
     def __init__(self, host='localhost', port=6379):
@@ -22,7 +80,7 @@ class Jet:
                 command_parts = parser.parse_command()
                 if command_parts:
                     command_name = command_parts.pop(0)
-                    command = CommandFactory.get_command(command_name, command_parts, self.store, self.expiry)
+                    command = CommandFactory.get_command(command_name, command_parts)
                     if command:
                         command.execute(writer)
                     else:
@@ -39,9 +97,4 @@ class Jet:
             ready_event.set()
 
         async with server:
-            await server.serve_forever()            
-
-
-if __name__ == "__main__":
-    jet = Jet()
-    asyncio.run(jet.run())
+            await server.serve_forever()
--- tests/test_command.py
+++ tests/test_command.py
@@ -1,4 +1,3 @@
-import time
 import socket
 from .utils import read_response, server
 
@@ -9,77 +8,5 @@ def test_ping_response(server):
         client.sendall(b"*1\r\n$4\r\nPING\r\n")
         response = read_response(client, b"+PONG\r\n")
         assert response == b"+PONG\r\n", f"Expected '+PONG\\r\\n', got '{response.decode()}'"
-    finally:
-        client.close()
-
-def test_multiple_ping_response(server):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 6379))
-    try:
-        client.sendall(b"*1\r\n$4\r\nPING\r\n*1\r\n$4\r\nPING\r\n")
-        response = read_response(client, b"+PONG\r\n+PONG\r\n")
-        assert response == b"+PONG\r\n+PONG\r\n", f"Expected '+PONG\\r\\n+PONG\\r\\n', got '{response.decode()}'"
-    finally:
-        client.close()
-
-def test_echo_response(server):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 6379))
-    try:
-        message = b"Hello, World!"
-        message_length = len(message)
-        client.sendall(b"*2\r\n$4\r\nECHO\r\n$" + str(message_length).encode() + b"\r\n" + message + b"\r\n")
-        expected_response = b"$" + str(message_length).encode() + b"\r\n" + message + b"\r\n"
-        response = read_response(client, expected_response)
-        assert response == expected_response, f"Expected '{expected_response.decode()}', got '{response.decode()}'"
-    finally:
-        client.close()
-
-def test_set_response(server):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 6379))
-    try:
-        # Sending SET command to store the value 'World' at key 'Hello'
-        client.sendall(b"*3\r\n$3\r\nSET\r\n$5\r\nHello\r\n$5\r\nWorld\r\n")
-        response = read_response(client, b"+OK\r\n")
-        assert response == b"+OK\r\n", f"Expected '+OK\\r\\n', got '{response.decode()}'"
-    finally:
-        client.close()
-
-def test_set_with_expiry(server):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 6379))
-    try:
-        # Set the value 'temp' at key 'expire_key' with an expiry of 1000 milliseconds (1 second)
-        client.sendall(b"*5\r\n$3\r\nSET\r\n$10\r\nexpire_key\r\n$4\r\ntemp\r\n$2\r\nPX\r\n$4\r\n1000\r\n")
-        set_response = read_response(client, b"+OK\r\n")
-        assert set_response == b"+OK\r\n", f"Expected '+OK\\r\\n' after SET with expiry, got '{set_response.decode()}'"
-
-        # Sleep for a duration longer than the expiry time to ensure the key has expired
-        time.sleep(1.5)  # Sleep for 1.5 seconds to ensure expiry has passed
-
-        # Attempt to get the value of 'expire_key', expecting it to have expired
-        client.sendall(b"*2\r\n$3\r\nGET\r\n$10\r\nexpire_key\r\n")
-        expected_get_response = b"$-1\r\n"  # This is the response when the key does not exist
-        get_response = read_response(client, expected_get_response)
-        assert get_response == expected_get_response, f"Expected '{expected_get_response.decode()}' for expired key, got '{get_response.decode()}'"
-    finally:
-        client.close()
-
-def test_get_response(server):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 6379))
-    try:
-        # First, set the value 'World' at key 'Hello'
-        client.sendall(b"*3\r\n$3\r\nSET\r\n$5\r\nHello\r\n$5\r\nWorld\r\n")
-        # Wait for the OK response from the SET command
-        set_response = read_response(client, b"+OK\r\n")
-        assert set_response == b"+OK\r\n", f"Expected '+OK\\r\\n' after SET, got '{set_response.decode()}'"
-
-        # Then, retrieve the value 'World' at key 'Hello'
-        client.sendall(b"*2\r\n$3\r\nGET\r\n$5\r\nHello\r\n")
-        expected_get_response = b"$5\r\nWorld\r\n"
-        get_response = read_response(client, expected_get_response)
-        assert get_response == expected_get_response, f"Expected '{expected_get_response.decode()}', got '{get_response.decode()}'"
     finally:
         client.close()
